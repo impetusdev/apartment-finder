40330b5b7a707de8743eaf0db29f32bac8451867
Laurence Qi <laurence.qi@outlook.com>
2023-01-04 08:21:25 +1100

    added basic url request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+
 fn main() {
-    println!("Hello, world!");
+    let url = "https://www.worldometers.info/coronavirus/";
+    let response = reqwest::blocking::get(url).expect("Could not load URL");
+
+    
+    let raw_html_string = response.text().unwrap();
 }
